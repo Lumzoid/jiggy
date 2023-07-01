390b89823677bfe90d8bd62e524312ed89a82073
Terraform-user <lumzoid@gmail.com>
2023-06-30 23:22:31 -0600

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
-    region = "us-east-1"
-    key = "eks/terraform.tfstate"
+   bucket = "oluabi-terr"
+    region = "ca-central-1"
+    key    = "eks/terraform.tfstate"
   }
 }
